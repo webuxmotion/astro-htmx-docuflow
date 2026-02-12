c88f4cb00371406cc9e6ed62ec804b7b3017ee7e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-02-12 16:35:23 +0200

    create and use layouts

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,12 @@
+// src/pages/api/hello.ts
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ request }) => {
+  return new Response(
+    JSON.stringify({ message: 'Привіт!' }),
+    {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    }
+  );
+};
